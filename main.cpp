203730469b76e6a1da84b453b8b6e3e2c69a594e
Kush Sharma <kushsharma2910@gmail.com>
2022-04-24 13:45:23 +0530

    isValid giving same output for default and valid vals

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include "kitepp.hpp"
 #include <iostream>
 
+constexpr double DEFAULTDOUBLE = std::numeric_limits<double>::quiet_NaN();
+
 int main(int argc, const char **argv)
 {
-  std::cout << kiteconnect::isValid(12);
+  std::cout << "isValid for default double :" << kiteconnect::isValid(DEFAULTDOUBLE) << std::endl;
+  std::cout << "isValid for 12.5 :" << kiteconnect::isValid(12.5) << std::endl;
   return 0;
 }
